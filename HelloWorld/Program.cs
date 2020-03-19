75bf694c0296352f58d718a32fde61f0fe3eeb63
Alec Isaacson <aisaacson@newrelic.com>
2020-03-19 12:24:33 -0400

    Intital commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//using NewRelic.LogEnrichers.NLog;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+     
+        static void Main(string[] args)
+        {
+            //var loggingConfig = new NLog.Config.LoggingConfiguration();
+
+            //var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "NRLog.json" };
+            //var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            //logfile.Layout = new NewRelicCsvLayout() { Delimiter = NLog.Layouts.CsvColumnDelimiterMode.Pipe };
+            
+            //loggingConfig.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logconsole);
+            //loggingConfig.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
+
+            //NLog.LogManager.Configuration = loggingConfig;
+
+            Console.WriteLine("Hello World!");
+            Logger.Info("I'm a log message");
+            Logger.Debug("I'm a debug message");
+        }
+    }
+}
